119d142014581f8de1fcb439917f198fb1fe330d
TeyyPro <143263970+teyypro@users.noreply.github.com>
2024-05-17 23:04:12 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter()
 	}
 };
-
-export default config;
